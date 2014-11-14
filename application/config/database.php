67acbc25e32756b10b9befafb28fafb2454207c7
Shivraj Chari <shivraj.goa@gmail.com>
2014-11-14 16:17:31 +0530

    #db changes

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'shivrajchari';
-$db['default']['password'] = 'rachna#sharma';
-$db['default']['database'] = 'shivrajc_demo';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'autocomplete';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
